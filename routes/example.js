12dc6a476d9dc12a3f53f7738cd5c7d03d685443
ugin <evgeniy.barannikov@brymonsoft.com>
2019-12-02 19:08:05 +0300

    Make 191202-1907

--- routes/example.js
+++ routes/example.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  res.send('<p>/example</p>');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h2>Welcome to "cognitive-server-echo" project</h2>' +
+    '<a href="/static">/static</a>' +
+    '</br></br><a href="/example">/example</a>'
+  );
 });
 
 module.exports = router;
